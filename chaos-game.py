0f9d9ccd3a1371649b2cb995b1df6b7cc6202e53
Anders Kielland <kielland@Anders-MacBook-Air.local>
2019-11-16 06:30:40 +0100

    iterate method added to chaos game

--- chaos-game.py
+++ chaos-game.py
@@ -48,14 +48,12 @@ class ChaosGame:
         # print(starting_point)
         return starting_point
 
-
     def plot_ngon(self):
         plt.scatter(*zip(*self.c))
 
         plt.axis("equal")
         #plt.axis("off")
        
-
     def plot_ngon_filled(self, X):
         plt.scatter(*zip(*X), s=0.2)#, marker=".")
 
@@ -63,14 +61,22 @@ class ChaosGame:
         #plt.axis("off")
 
     def iterate(self, steps, discard=5):
-        
-        
-        
+        r = self.r
+        self.X = np.empty((steps, 2))
+        for i in range(steps-1):
+            c = self._starting_point()
+            self.X[i+1] = r * self.X[i] + (1-r) * c
+
+        self.X = np.delete(self.X, (0, discard), axis=0)
+
 
 if __name__ == "__main__":
     test = ChaosGame(5)
+    test.iterate(10)
     
-    N = 100_000
+
+    """
+    N = 1000
     X = np.empty((N, 2))
     for i in range(N):
         X[i] = test._starting_point()
@@ -78,4 +84,5 @@ if __name__ == "__main__":
     test.plot_ngon_filled(X)
     test.plot_ngon()
     plt.show()
+    """
 
